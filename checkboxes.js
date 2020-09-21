2cb9c213e3fbd0d25658a6d0535692c468709af5
Giovanni <desiderio.giovanni92@gmail.com>
2020-09-21 16:11:31 +0200

    inizio

---
+++ checkboxes.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function(){   
+   
+    var progLanguages = ['C', 'C++', 'JAVA', 'PHYTON', 'SWIFT', 'PHP', 'C SHARP'];
+var cont = 0; 
+totLang = progLanguages.length;
+var formID = document.getElementById('idDati');
+var htmlContent = ''; 
+for (; cont < totLang; cont++){ 
+htmlContent += '<div><input type="checkbox" value='+progLanguages[cont]+'">'+progLanguages[cont] +'</div>'; 
+}
+console.log(htmlContent);
+formID.innerHTML = htmlContent; 
+});
---
+++ espressione-istruzione.js
@@ -0,0 +1,44 @@
+
+//This is a comment
+/*
+    This is a lesson 
+    about javascript language
+ */
+
+console.log("first1 ',' output1");
+
+console.log('first2 \':\' output2');
+
+console.log("first3 ', 'output3");
+
+console.log("first4 \": \" output4");
+
+console.log(2+3);
+
+console.log('Hello World');
+
+
+/**
+ * @var age number
+ * 
+ * 
+ * 
+ */
+
+ //Stiamo assegnando questo valore ad un area di memoria del computer(RAM) che identifichiamo con age
+ //We are assigning this value to a computer memory area (RAM) that we identify with 'age'
+var age = 28;
+console.log(age);
+
+var name = 'Giovanni';
+console.log(name, ' ha ', age, ' anni ');
+console.log(name + ' ha ' + age + ' anni ');
+
+console.clear();
+
+if(age < 30){
+    console.log("You are younger than 30");
+} else {
+    console.log('You are older than 30');
+}
+
